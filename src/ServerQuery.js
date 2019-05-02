951167a0e4067d23228a54ed473e9b5cfbfe9fca
planethouki <26595148+planethouki@users.noreply.github.com>
2019-05-02 13:33:11 +0900

    0.1.0

---
+++ src/ServerQuery.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+export default class ServerQuery extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            data: {
+                version: null,
+                players: null
+            },
+        };
+    }
+
+    componentDidMount() {
+        fetch("https://houkiserverquery.azurewebsites.net/")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        data: result
+                    });
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
+    render() {
+        const { error, isLoaded, data } = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+                <ul>
+                    <li>アドレス・・・planethouki.ddns.net</li>
+                    <li>バージョン・・・<span id="version">{data.version.name}</span></li>
+                    <li>ユーザー・・・<span id="login-players">{data.players.online}</span>／<span id="max-players">{data.players.max}</span></li>
+                </ul>
+            );
+        }
+    }
+}
