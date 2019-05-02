44609c39d45c6dcc6838e926b1758013fbddf47b
planethouki <26595148+planethouki@users.noreply.github.com>
2019-05-02 13:32:19 +0900

    react

--- main.js
+++
@@ -1,6 +0,0 @@
-$.get("https://houkiserverquery.azurewebsites.net/", function(data){
-	$("#version").text(data.version.name);
-	$("#max-players").text(data.players.max);
-	$("#login-players").text(data.players.online);
-	// $("#motd").text(data.description.text);
-});
