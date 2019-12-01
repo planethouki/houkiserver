50bcda648a150df88fdbb9a93876f1680c09f033
planethouki <26595148+planethouki@users.noreply.github.com>
2019-12-01 22:16:19 +0900

    Updates

---
+++ precache-manifest.acd865c9a2c8b803a7575df7ee033972.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d19c03ac87ca2267429b6ad9e4e8f6e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d98a6aa0c1cb878572c3",
+    "url": "/static/css/main.44abc40e.chunk.css"
+  },
+  {
+    "revision": "cf6f8d05b62dc8516a65",
+    "url": "/static/js/2.a5feaefa.chunk.js"
+  },
+  {
+    "revision": "d98a6aa0c1cb878572c3",
+    "url": "/static/js/main.b57aff5a.chunk.js"
+  },
+  {
+    "revision": "4c4e6e4a090675f64372",
+    "url": "/static/js/runtime-main.e8b175bf.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/houkiserver/precache-manifest.5b2997f47f0bf36c17678c64c3005e2c.js"
+  "/precache-manifest.acd865c9a2c8b803a7575df7ee033972.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/houkiserver/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
