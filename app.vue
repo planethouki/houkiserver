62ef1c1981fc112e403660475aac3242df1bf34e
planethouki <26595148+planethouki@users.noreply.github.com>
2023-03-09 21:47:50 +0900

    del footer

--- app.vue
+++ app.vue
@@ -37,24 +37,6 @@
 
       <NuxtPage/>
 
-      <div class="bg-light pt-3">
-        <div class="d-flex flex-column flex-sm-row small px-3">
-          <div class="d-flex flex-column" style="min-width: 8rem;">
-            <h5>ほうき鯖</h5>
-          </div>
-          <div class="d-flex flex-column pb-3" style="min-width: 10rem;">
-            <nuxt-link to="rules">
-              ルール
-            </nuxt-link>
-            <nuxt-link to="howto">
-              ハウツー
-            </nuxt-link>
-            <nuxt-link to="ranking">
-              ランキング
-            </nuxt-link>
-          </div>
-        </div>
-      </div>
     </NuxtLayout>
   </div>
 </template>
