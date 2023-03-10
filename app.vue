d8cf15c5e95846dcab108d7247d41da95726f44d
planethouki <26595148+planethouki@users.noreply.github.com>
2023-03-10 09:30:48 +0900

    navbar never collapse

--- app.vue
+++ app.vue
@@ -1,25 +1,13 @@
 <template>
   <div>
     <NuxtLayout>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <nav class="navbar navbar-expand navbar-light bg-light">
         <div class="container-fluid">
           <nuxt-link class="navbar-brand" to="/">
             ほうき鯖
           </nuxt-link>
 
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <div class="" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
                 <nuxt-link class="nav-link active" aria-current="page" to="/ranking">ランキング</nuxt-link>
