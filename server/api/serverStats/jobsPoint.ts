c1bcc454fd424dce0549ad75dacb2ea0172f7ce9
planethouki <26595148+planethouki@users.noreply.github.com>
2023-03-10 09:07:33 +0900

    server stats api

---
+++ server/api/serverStats/jobsPoint.ts
@@ -0,0 +1,9 @@
+export default defineEventHandler(async (event) => {
+  const headers = {
+    'Content-Type': 'application/json'
+  }
+
+  setHeader(event, 'Content-Type', 'application/json')
+
+  return $fetch('https://houkiserverstats.z31.web.core.windows.net/jobs_point.json', { headers })
+})
